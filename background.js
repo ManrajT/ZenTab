0055a4ed6e8a11de34c3c4178df3310951b8314a
Manraj <ManrajT@gmail.com>
2013-06-29 14:58:03 -0400

    Changes to background.js

--- background.js
+++ background.js
@@ -1,19 +1,30 @@
 /*Problems:
   1. Making port between popup and background
-  2. If asychronous functions work with objects like normal
-  3. Integrating flags for certain events with my check function
+  2. If asychronous functions work with objects as expected
+  3. Finding way to reset timers and restart timers when onActivated event fires
 */ 
 chrome.runtime.onInstalled.addListener(function(detailsObject){
-  if(detailsObject.reason == "install"
+  if(detailsObject.reason == "install")
 	localStorage["timeout"] = 5; 		//Set initival value of timeout
 }
 
-chrome.runtime.onStartup.addListener(function
+chrome.tab.onCreated.addListener(function(tab){
+  var timebomb = new Timebomb(tab, localStorage["timeout"]);
+}; 
+
+chrome.tab.onUpdated.addListener(function(tabId, changeInfo){
+  
+
+chrome.tab.onActivated.addListener(function(activeInfo){
+
 //Timebomb object for removing tabs after period of time
+
+
 var Timebomb = function(tab, timeout){
   this.tab = tab;
   this.timeout = timeout;
   this.timerStart = Date.now; 
+  var clearCheckID = 0;  //ID necessary for resetting timer
   
   this.stop() = function(){
     clearInterval(clearCheckID);
@@ -29,7 +40,7 @@ var Timebomb = function(tab, timeout){
   };
   
   this.check = function(){
-    var clearCheckID = setInterval(function(){
+    clearCheckID = setInterval(function(){
 		if(Date.now > (timerStart + timeout)) 
 	  
   
