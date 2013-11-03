da099dbeec2b221011bc81e941b57ddbb7a23194
Manraj Tatla <ManrajT@gmail.com>
2013-11-02 22:43:20 -0400

    playing around with API

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
 //Peak into what this is doing through alerts and inspect views console
+//onCreated + lastFocused workaround + remove + update 
 
+//Storing data of currently active tab
+chrome.tabs.onActivated.addListener(function(activeInfo){
+  alert("Activated tab has the id:" + activeInfo.tabId); 
+});
+
+ 
+//Passing over that data onCreated
  
 
 
