87cc28fe3c08e663e2509a02e6df6c3d3d135410
Manraj Tatla <ManrajT@gmail.com>
2013-11-16 11:35:46 -0500

    redirect works for chrome:// settings

--- background.js
+++ background.js
@@ -10,9 +10,9 @@ function tabIdArrayFrom(tabArray){
 
 function startUp(){
   chrome.tabs.query({}, function(tabArray){
-     var startLength  = tabArray.length;
      if(tabArray.length > 1){
        var startResponse = confirm("Would you like enable ZenTab?\nThis will close all your tabs");
+       
        if(startResponse == true){
          chrome.tabs.query({active:false}, function(inactiveTabs){  
            chrome.tabs.remove(tabIdArrayFrom(inactiveTabs)); 
@@ -21,24 +21,24 @@ function startUp(){
            chrome.tabs.remove(tabIdArrayFrom(tabsInOtherWindows)); 
          })
        }
+       
        else if(startResponse == false){
          chrome.management.setEnabled(chrome.runtime.id, false);
        }
      }
+     
      chrome.tabs.query({active:true, windowType:"normal"}, function(tabArray){
-       mainTabId = tabArray[0].id;    //mainTabId is not being set 
+       mainTabId = tabArray[0].id; 
      })
   })
 }
 
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-  if(tabId == mainTabId){
-    mainTabId = undefined;
-  }
-})
-
 chrome.tabs.onCreated.addListener(function(tab){
-  if(/New\sTab/.test(tab.title)){	//Prevent new tab from being opened
+  if(/chrome:/.test(tab.url)){
+	chrome.tabs.update(mainTabId,{url:tab.url, active:true});
+	chrome.tabs.remove(tab.id);   	
+  }
+  else if(/New\sTab/.test(tab.title)){	//Prevent new tab from being opened
     chrome.tabs.remove(tab.id); 
   }
   else if(mainTabId == undefined){   //Redo startup() if mainTab's been closed
@@ -48,13 +48,22 @@ chrome.tabs.onCreated.addListener(function(tab){
     chrome.tabs.remove(tab.id); 
 })
 
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
+  if(tabId == mainTabId){
+    mainTabId = undefined;
+  }
+})
+
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
   if(changeInfo.url == "chrome://newtab" && tabId == mainTabId){
     startUp();
   }
 })
 
-//Redirect links opening in new tabs back to the main tab
+
+//Open links opening in new tab in the mainTab
 chrome.webNavigation.onBeforeNavigate.addListener(function(details){
   if(details.tabId != mainTabId && details.frameId == 0){
     chrome.tabs.update(mainTabId, {url:details.url, active:true});
